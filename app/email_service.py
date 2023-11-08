52be25424a5dc23ea86706fd78cee84be15a96f2
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-11-08 17:38:25 -0500

    Added testing feature for email service

--- app/email_service.py
+++ app/email_service.py
@@ -31,6 +31,7 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
         print(response.status_code) #> 202 indicates SUCCESS
         print(response.body)
         print(response.headers)
+        return response.status_code
 
     except Exception as err:
         print(type(err))
