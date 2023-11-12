ee3ab622187aef20289a2cc8f18a04527b25ac38
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-11-12 15:37:26 -0500

    More testing for unemployment file

---
+++ test/unemployment_test.py
@@ -0,0 +1,8 @@
+from app.unemployment import fetch_data
+
+def test_fetch_data():
+    data = fetch_data()
+    assert isinstance(data, list)
+    assert len(data) > 100
+    assert isinstance(data[0], dict)
+    assert list(data[0].keys()) == ["date", "value"]
