fdce46cfd59ab8a3bbd485f125241df86554654d
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-11-29 16:19:00 -0500

    Implemented unemployment dashboard function

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,14 +1,19 @@
 # this is the "web_app/__init__.py" file...
 
 from flask import Flask
+import os
 
 from web_app.routes.home_routes import home_routes
+from web_app.routes.unemployment_routes import unemployment_routes
 #from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
+    app.register_blueprint(unemployment_routes)
     #app.register_blueprint(book_routes)
     #app.register_blueprint(weather_routes)
     return app
